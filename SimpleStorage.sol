00ea9098705fec0d89a264739d541497c55002e1
YUBIN ARSENE <169004887+ARSENE101@users.noreply.github.com>
2026-08-22 02:12:03 +0100

    Rename favoriteNumber and add person management

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -3,9 +3,22 @@ pragma solidity ^0.8.30;
 
 contract SimpleStorage{
 
-        uint256 public favoriteNumber;
+        uint256 public myFavoriteNumber;
 
+        struct Person //this is a way to create ones custom types
+        {
+           uint256 favoriteNumber;
+           string name;     // now in a custom type you have to pass in the types 
+                           // of your custom types say arguements what can be in the custom type and what cant.
+                          //  now custom  types count like arrays from 0 - m+, e.g here favnumber is 0 and name = 
 
+        } 
+
+                    Person public pat =Person(7,"pat"); // thesame =      Person public pat = Person({favoriteNumber: 7 name: "pat" })
+// now say you want to actually have an array of people
+Person[] public ListOfPeople; // this right here helps us do just that, create a lists or an array for person permiting person to be able to add other peple like we will do.
+
+        
 // Noting there are 2 states of functions, a function 
 //that is actively changing the state of the blockchain, 
 //that is updating and doing a transaction, etc for example
@@ -16,7 +29,7 @@ contract SimpleStorage{
 
 
     function store(uint256 _favoriteNumber) public {
-            favoriteNumber =  _favoriteNumber;
+            myFavoriteNumber =  _favoriteNumber;
     }
 // again a function can be in a view or pure state, view means it
 // can read but cannot change state of the blockchain in whatsoever way
@@ -27,7 +40,11 @@ contract SimpleStorage{
 // actually dont cost gas except they are being calle by another function that actually does affect the state of the blockchain
 // for example our store function here calling our retrieve function (return retrieve; or just retrieve;) 
     function retrieve() public view returns(uint256){
-        return favoriteNumber;
+        return myFavoriteNumber;
     }
 
+
+    function AddPerson(string memory _name, uint256 _favoriteNumber )public  {
+            ListOfPeople.push(Person(_favoriteNumber, _name));
+    }
 }
