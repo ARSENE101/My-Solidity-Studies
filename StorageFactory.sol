cc3cc4035145427baf2e6623796caa70c6e5519d
YUBIN ARSENE <169004887+ARSENE101@users.noreply.github.com>
2026-08-26 00:48:14 +0100

    Implement multiple SimpleStorage contract management
    Added functionality to create and manage multiple SimpleStorage contracts.

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -7,11 +7,30 @@ import {SimpleStorage} from "./SimpleStorage.sol"; // import formating precises 
 
 
 contract StoragreFactory{
-    SimpleStorage public simpleStorage;
-    function createSimpleStorageContracth() public {
-        simpleStorage = new SimpleStorage();
+    //stores the contracts we create or transact in an array
+     SimpleStorage[] public listOfSimpleStorageContracts;
+   
+    function createSimpleStorageContract() public {
+        
+        SimpleStorage newSimpleStorageContract = new SimpleStorage();
+        listOfSimpleStorageContracts.push(newSimpleStorageContract);// push the contracts adress or reference into the list
+    
     }
 
-}
+    function sfStore (uint256 _simpleStorageIndex, uint256 _newSimpleStorageNumber) public  {
+        // Get the SimpleStorage contract reference stored at this array index and assign it to mySimpleStorage
+        // let SimpleStorage mySimpleStorage = x, ==> x = listOfSimpleStorageContracts[_simpleStorageIndex]
+        // now what is listOfSimpleStorageContracts[_simpleStorageIndex]? the array of listOfSimpleStorageContracts created above
+        // so any contract created by the CreateSimpleStorage function, retains an adress and that adress has an index in our function below = _simpleStorageIndex. 
+        SimpleStorage mySimpleStorage = listOfSimpleStorageContracts[_simpleStorageIndex];
+        mySimpleStorage.store(_newSimpleStorageNumber); // stores this number in the contract with the index value 
+    }
+    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
+        SimpleStorage mySimpleStorage = listOfSimpleStorageContracts[_simpleStorageIndex];
+        return mySimpleStorage.retrieve();
+    }
+
+    }
+
 
 
