d82af9481b5dbf845675a3741c01a49f7d09f3e6
YUBIN ARSENE <169004887+ARSENE101@users.noreply.github.com>
2026-09-01 23:53:01 +0100

    Enhance FundMe contract with price and conversion functions
    Added functions to get price and conversion rate.

--- FundMe.sol
+++ FundMe.sol
@@ -1,17 +1,33 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30;
 
+
+
 contract FundMe{
+
+    uint256 minimumUsd = 5;
     //Target allow users to be able to send money
     // set a minimum amount to fund
-    uint256 public myValue  = 1;
 
     function fund()  public payable {// the payable key word allows the contyract to look red in our contract ==> it is a payable function 
-    myValue = myValue + 2;
     require(msg.value > 1e18, "Can't send due to insuffecient ETH"); //1e18 = 1e18wei ==> 100000000000000000 <=> 10^18wei ==> 1ETH
 
     }
 
     //function withdraw() public {}
+    function getPrice() public { // we do not know the price of usd in the blockxchain so we use an arbitrary tool like chainlink to know the decentralixed usd price in real time
+    //Adress
+    //ABI
+
+    
+
+
+
+    }
+
+    function getConvertionRate() public {
+
+
+    }
 
 }
