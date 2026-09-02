fe829f9fe978c324c59c485df8240357be1a09df
YUBIN ARSENE <169004887+ARSENE101@users.noreply.github.com>
2026-09-02 21:04:16 +0100

    Integrate Chainlink Aggregator interface
    Added AggregatorV3Interface for Chainlink price feeds and implemented getVersion function.

--- FundMe.sol
+++ FundMe.sol
@@ -1,7 +1,30 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30;
 
+//the code taken from https://github.com/smartcontractkit/chainlink-evm/blob/develop/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol?utm_source=chatgpt.com
+// their(chainlink) updated aggregatorinterface code we need for the ABI (Bunch of functions our contract will use to interact fluently with other contracts)
 
+
+// solhint-disable-next-line interface-starts-with-i
+interface AggregatorV3Interface {
+  function decimals() external view returns (uint8);
+
+  function description() external view returns (string memory);
+
+  function version() external view returns (uint256);
+
+  function getRoundData(
+    uint80 _roundId
+  ) external view returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
+
+  function latestRoundData()
+    external
+    view
+    returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
+}
+
+
+// the agregator code 
 
 contract FundMe{
 
@@ -16,17 +39,21 @@ contract FundMe{
 
     //function withdraw() public {}
     function getPrice() public { // we do not know the price of usd in the blockxchain so we use an arbitrary tool like chainlink to know the decentralixed usd price in real time
-    //Adress
-    //ABI
+    //Adress 0x694AA1769357215DE4FAC081bf1f309aDC325306
+    //ABI = agreagtorinterface
 
     
-
-
 
     }
 
     function getConvertionRate() public {
 
+
+    }
+
+    function getVersion() public view returns (uint256) {
+
+      return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version(); // this line uses the agregatorinterface code to interact with that chain address.
 
     }
 
